2ee0ab6dbfd8fea224e9d3b0d46f1f035dc02210
vrossi96 <vrossi396@yahoo.it>
2022-02-04 14:36:42 +0100

    show email list when its completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /* 
 Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
-Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+|Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
-Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+|Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 Evitare i doppioni nella lista delle mail 
 */
 
