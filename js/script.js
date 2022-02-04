c8057669e1d252476297d6694c211fbefd4c3c98
vrossi96 <vrossi396@yahoo.it>
2022-02-04 14:23:39 +0100

    add array for email

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* 
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+Evitare i doppioni nella lista delle mail 
+*/
+
+const app = new Vue({
+   el: "#root",
+   data: {
+      emails: [],
+   },
+});
